70dd34786acbcb090e7b16f08494c284751c04ea
Scott DePouw <scott@ardalis.com>
2018-08-01 09:42:44 -0400

    Basic specs for BaseEntity and TodoItem.

---
+++ src/CleanArchitecture.Core/Specifications/BaseEntitySpec.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+using CleanArchitecture.Core.SharedKernel;
+
+namespace CleanArchitecture.Core.Specifications
+{
+    public class BaseEntitySpec<T> : BaseSpecification<T> where T : BaseEntity
+    {
+        public BaseEntitySpec(Expression<Func<T, bool>> criteria) : base(criteria)
+        { }
+
+        public static BaseEntitySpec<T> ById(int id) => new BaseEntitySpec<T>(entity => entity.Id == id);
+    }
+}
---
+++ src/CleanArchitecture.Core/Specifications/ToDoItemSpec.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Core.Entities;
+using System;
+using System.Linq.Expressions;
+
+namespace CleanArchitecture.Core.Specifications
+{
+    public class ToDoItemSpec : BaseSpecification<ToDoItem>
+    {
+        public ToDoItemSpec(Expression<Func<ToDoItem, bool>> criteria) : base(criteria)
+        { }
+
+        public static ToDoItemSpec ByIsDone(bool isDone) => new ToDoItemSpec(todoItem => todoItem.IsDone == isDone);
+        public static ToDoItemSpec ByTitle(string title) => new ToDoItemSpec(todoItem => todoItem.Title == title);
+    }
+}
