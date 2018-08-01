aca243e779fa58bf9d1ba6268e6e42b070bb9864
Scott DePouw <scott@ardalis.com>
2018-08-01 09:42:33 -0400

    Basic ISpecification/BaseSpecification interface/class.

---
+++ src/CleanArchitecture.Core/Interfaces/ISpecification.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Core.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace CleanArchitecture.Core.Interfaces
+{
+    public interface ISpecification<T> where T : BaseEntity
+    {
+        Expression<Func<T, bool>> Criteria { get; }
+        string CacheKey { get; }
+        List<Expression<Func<T, object>>> Includes { get; }
+        List<string> IncludeStrings { get; }
+    }
+}
---
+++ src/CleanArchitecture.Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,38 @@
+﻿using Ardalis.GuardClauses;
+using CleanArchitecture.Core.Interfaces;
+using CleanArchitecture.Core.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CleanArchitecture.Core.Specifications
+{
+    public abstract class BaseSpecification<T> : ISpecification<T>
+        where T : BaseEntity
+    {
+        public Expression<Func<T, bool>> Criteria { get; }
+
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+        public List<string> IncludeStrings { get; } = new List<string>();
+        public string CacheKey { get; private set; }
+
+        protected BaseSpecification(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+
+        protected void AddInclude(Expression<Func<T, object>> includeExpression) => Includes.Add(includeExpression);
+        protected void AddInclude(string includeString) => IncludeStrings.Add(includeString);
+        public bool ShouldCache => !string.IsNullOrWhiteSpace(CacheKey);
+
+        protected void EnableCache(string specMethodName, params object[] args)
+        {
+            Guard.Against.NullOrWhiteSpace(specMethodName, nameof(specMethodName));
+
+            List<string> keyNames = new List<string> { typeof(T).Name, specMethodName };
+            keyNames.AddRange(args.Select(arg => arg?.ToString() ?? string.Empty));
+            CacheKey = string.Join("-", keyNames);
+        }
+    }
+}
